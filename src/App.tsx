76460e738f79193b151a5fb084bf6112cb260ebb
Sergey <Sergey.rimsha@gmail.com>
2021-07-26 21:43:57 +0300

    +react_router_dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Transform from './components/transform';
+import Transform from './components/Transform/transform';
+import Game from './components/Game/Game';
 
 function App() {
     return (
         <div className="App">
-            <Transform />
+            <Router>
+                <div>
+                    <Link to="/transform">Transform</Link>
+                </div>
+                <div>
+                    <Link to="/game">Game</Link>
+                </div>
+
+                <Switch>
+                    <Route path="/transform">
+                        <Transform/>
+                    </Route>
+                    <Route path="/game">
+                        <Game/>
+                    </Route>
+                </Switch>
+            </Router>
+            {/* <Transform /> */}
         </div>
     );
 }
---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Game = () => {
+    
+    return (
+        <div>
+            This my first game:)
+            maby laters....
+        </div>
+    )
+}
+
+export default Game;
